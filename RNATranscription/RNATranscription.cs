4772cee43962cd0f070e141d206b3239fd427df0
Varun Malik <varun1malik@gmail.com>
2018-07-19 15:10:04 +0530

    rna solution

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace RNATranscriptionModule
 {
@@ -6,7 +7,18 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            Dictionary<char, char> rna_replace = new Dictionary<char, char>(){
+                {'G','C'}, {'C','G'}, {'T','A'}, {'A','U'}
+            };
+            char [] rna_char = nucleotide.ToCharArray();
+            for(int i=0; i<rna_char.Length;i++)
+            {
+                char newstrand;
+                if (rna_replace.TryGetValue(nucleotide[i], out newstrand))
+                rna_char[i] = newstrand;
+            }
+            string new_nucleotide = new string(rna_char);
+            return new_nucleotide;
         }
     }
 }
